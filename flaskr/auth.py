2c940583ae4b9c815a119a43ca88c6f6e0321a20
czarek1110895 <cezary.rolka95@gmail.com>
2024-01-13 20:14:16 +0100

    refactor columns in db & add some validation to register

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,4 +1,5 @@
 import functools
+import re
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
@@ -9,30 +10,50 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+valid_email = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+valid_pswd = r'^.{3,}$'
+
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
+        name = request.form['name']
         username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
         db = get_db()
         error = None
 
-        if not username:
+        # Check if all required fields are filled in
+        if not name:
+            error = 'name is required.'
+        elif not username:
             error = 'Username is required.'
+        elif not email:
+            error = 'Email is required.'
         elif not password:
             error = 'Password is required.'
+
+        # Check if email and password match the pattern
+        if not check_if_match_pattern(valid_email, email):
+            error = "Invalid email."
+        elif not check_if_match_pattern(valid_pswd, password):
+            error = "Invalid password."
 
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
+                    "INSERT INTO user (name, username, email, password) VALUES (?, ?, ?, ?)",
+                    (name, username, email, generate_password_hash(password)),
                 )
                 # commit() needs to be called to save the changes.
                 db.commit()
+            # Check if used email and username are already in db
             except db.IntegrityError:
-                error = f"User {username} is already registered."
+                if check_if_value_exist_in_db('username', username):
+                    error = f"User {username} is already registered."
+                elif check_if_value_exist_in_db('email', email):
+                    error = f"Email {email} is already registered."
             else:
                 return redirect(url_for("auth.login"))
 
@@ -98,3 +119,17 @@ def login_required(view):
         return view(**kwargs)
 
     return wrapped_view
+
+
+def check_if_match_pattern(regex, obj):
+    return True if re.fullmatch(regex, obj) else False
+
+
+def check_if_value_exist_in_db(key, value):
+    db = get_db()
+    query = f"SELECT * FROM user WHERE {key}='{value}'"
+    result = db.execute(query).fetchone()
+
+    return True if result is not None else False
+
+
